6e2666af37bb1be375492f22fc3144a8774c3621
nac19 <74272103+nac19@users.noreply.github.com>
2023-08-07 01:05:08 +0300

    Added project deployment function

--- contracts/main.sol
+++ contracts/main.sol
@@ -2,17 +2,21 @@
 pragma solidity ^0.8.9;
 
 contract Main {
+    
+    Project[] public projects;
+    mapping(uint256 => WithdrawalRequest[]) requests;
 
     struct Project {
         uint256 id;
         address projectOwner;
-        bytes32 EAS_UID;
+        //bytes32 EAS_UID;
         string projectName;
         uint256 supporterCount;
         uint256 donationAmount;
         uint256 goalAmount;
+        uint256 withdrawalRequestCount;
+        ProjectNFT projectNFT;
         bool isActive;
-        WithdrawalRequest[] requests;
     }
 
     struct WithdrawalRequest{
@@ -24,5 +28,44 @@ contract Main {
         bool isActive;
     }
 
-    Project[] projects;
+    struct ProjectNFT {
+        //address nftAddress;
+        uint256 threshold;
+        uint256 maxSupply;
+        uint256 ownerCount;
+    }
+
+    function deployNewProject(
+        //bytes32 _EAS_UID,
+        string calldata _projectName,
+        uint256 _goalAmount,
+        uint256 _nftThreshold,
+        uint256 _nftMaxSupply,
+        string calldata _nftShort
+    ) external returns (uint256) {
+
+        uint256 currentID = projects.length;
+
+        // NFT Deployment (nftAddress)
+
+        projects.push(Project({
+            id: currentID,
+            projectOwner: msg.sender,
+            //EAS_UID: _EAS_UID,
+            projectName: _projectName,
+            supporterCount: 0,
+            donationAmount: 0,
+            goalAmount: _goalAmount,
+            withdrawalRequestCount: 0,
+            projectNFT: ProjectNFT({
+                //nftAddress: nftAddress,
+                threshold: _nftThreshold,
+                maxSupply: _nftMaxSupply,
+                ownerCount: 0
+            }),
+            isActive: true
+        }));
+
+        return currentID;
+    }
 }
