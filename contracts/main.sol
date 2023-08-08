67e94edc44c829fdac248a6dd44a6152c1fbd7db
nac19 <74272103+nac19@users.noreply.github.com>
2023-08-08 20:00:05 +0300

    Abuse prevention

--- contracts/main.sol
+++ contracts/main.sol
@@ -15,6 +15,7 @@ contract Main {
     mapping(uint256 => WithdrawalRequest[]) requests;
     mapping(uint256 => mapping(uint256 => mapping(address => bool))) voteStatus;
     uint256 constant minSecondsToVote = 1209600;
+    uint8 constant maxFailedWithdrawalRequests = 3;
 
     constructor(){
         SMILE = new SmileProtocolToken("Smile Protocol", "SMILE");
@@ -29,6 +30,7 @@ contract Main {
         uint256 totalDonationAmount;
         uint256 goalAmount;
         uint256 withdrawalRequestCount;
+        uint256 failedWithdrawalRequestCount;
         ProjectNFT projectNFT;
         bool isActive;
     }
@@ -71,6 +73,7 @@ contract Main {
             totalDonationAmount: 0,
             goalAmount: _goalAmount,
             withdrawalRequestCount: 0,
+            failedWithdrawalRequestCount: 0,
             projectNFT: ProjectNFT({
                 nftAddress: address(nftContract),
                 threshold: _nftThreshold,
@@ -122,35 +125,49 @@ contract Main {
 
     function createWithdrawalRequest(uint256 _projectID, uint256 _amount, uint256 _endTimestamp, string memory _description) external {
         require(msg.sender == projects[_projectID].projectOwner, "You are not authorized");
+        require(projects[_projectID].isActive, "This project is no longer active.");
         require(_amount > 0, "The withdrawal amount must be greater than 0.");
-        require(block.timestamp > requests[_projectID][(projects[_projectID].withdrawalRequestCount)].endTimestamp, "There is already an active withdrawal request.");
+        require(block.timestamp > checkLastWithdrawalRequest(_projectID).endTimestamp, "There is already an active withdrawal request.");
         require(_amount <= projects[_projectID].currentBalance, "Unvalid amount.");
         require(_endTimestamp >= (block.timestamp + minSecondsToVote), "There must be a minimum 14 days voting period.");
 
-        requests[_projectID].push(WithdrawalRequest({
-            requestID: projects[_projectID].withdrawalRequestCount,
-            //failed withdrawal request count to project struct
+        if(checkLastWithdrawalRequest(_projectID).declines > checkLastWithdrawalRequest(_projectID).approvals){
+            projects[_projectID].failedWithdrawalRequestCount++;
+        } else if (projects[_projectID].failedWithdrawalRequestCount > 0) {
+            projects[_projectID].failedWithdrawalRequestCount = 0;
+        }
 
+        if(projects[_projectID].failedWithdrawalRequestCount == maxFailedWithdrawalRequests) {
+            projects[_projectID].isActive = false;
+            return;
+        } else {
+            requests[_projectID].push(WithdrawalRequest({
+            requestID: projects[_projectID].withdrawalRequestCount,
             amount: _amount,
             approvals: 0,
             declines: 0,
             endTimestamp: _endTimestamp,
             description: _description
-        }));
+            }));
 
-        emit newWithdrawalRequest(
-            _projectID,
-            projects[_projectID].withdrawalRequestCount,
-            projects[_projectID].projectName,
-            _amount,
-            _description,
-            _endTimestamp
-        );
+            emit newWithdrawalRequest(
+                _projectID,
+                projects[_projectID].withdrawalRequestCount,
+                projects[_projectID].projectName,
+                _amount,
+                _description,
+                _endTimestamp
+            );
+
+            projects[_projectID].withdrawalRequestCount++;
+        }
     }
 
-    /*
-    function getNFTPower(uint256 _projectID, uint256 _id) external view returns(uint256){
-        return SmileProtocol_ProjectNFT(projects[_projectID].projectNFT.nftAddress).getNFTPower(msg.sender);
+    function checkWithdrawalRequest(uint256 _projectID, uint256 _reqID) external view returns(WithdrawalRequest memory) {
+        return requests[_projectID][_reqID];
     }
-    */
+
+    function checkLastWithdrawalRequest(uint256 _projectID) public view returns(WithdrawalRequest memory) {
+        return requests[_projectID][(projects[_projectID].withdrawalRequestCount)-1];
+    }
 }
